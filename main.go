2d1c0c74b36b58e2eb2c008dafe815d4dc2a1c4b
MiyuYokoyama0109 <miyu.y@latona.io>
2022-04-29 12:58:23 +0900

    release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-sales-order-reads/SAP_API_Caller"
-	"sap-api-integrations-sales-order-reads/sap_api_input_reader"
+	"sap-api-integrations-sales-order-reads/SAP_API_Input_Reader"
 
 	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
